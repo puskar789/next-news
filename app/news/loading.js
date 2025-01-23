d5d9a97b0ce2f82a1467b412f2f15c1d5e880189
puskar789 <pbohara231159@gmail.com>
2025-01-23 23:38:52 +0530

    added the news page

---
+++ app/news/loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewsPageLoading = () => {
+  return <p>Loading...</p>;
+};
+
+export default NewsPageLoading;
---
+++ app/news/page.js
@@ -0,0 +1,15 @@
+import NewsList from "@/components/NewsList";
+import { getAllNews } from "@/lib/news";
+
+const NewsPage = async () => {
+  const news = await getAllNews();
+
+  return (
+    <>
+      <h1>News Page</h1>
+      <NewsList news={news} />
+    </>
+  );
+};
+
+export default NewsPage;
