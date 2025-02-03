218882e6d72bf804fde4422bad6acb203d3424f9
puskar789 <pbohara231159@gmail.com>
2025-02-03 23:55:51 +0530

    added default page

---
+++ app/archive/@latest/default.js
@@ -0,0 +1,14 @@
+import NewsList from "@/components/NewsList";
+import { getLatestNews } from "@/lib/news";
+
+const LatestNewsPage = async () => {
+  const latestNews = await getLatestNews();
+  return (
+    <>
+      <h2>Latest News</h2>
+      <NewsList news={latestNews} />
+    </>
+  );
+};
+
+export default LatestNewsPage;
